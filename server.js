e908bf47e3cb2461d04aaeef0f61a45a65af53dd
SWAPNIL SINGH CHAUHAN <swap.2504@gmail.com>
2018-04-07 21:08:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
 
 
 app.get('/article-one', function (req, res) {
-  res.send('article one requesed and will be served here');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 
