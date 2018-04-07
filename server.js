ac45134487753d20881029b51202b399caa7590c
SWAPNIL SINGH CHAUHAN <swap.2504@gmail.com>
2018-04-07 20:58:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,24 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  
 });
+
+
+app.get('/article-one', function (req, res) {
+  res.send('article one requesed and will be served here');
+});
+
+
+app.get('/article-two', function (req, res) {
+  res.send('article two requesed and will be served here');
+});
+
+app.get('/article-three', function (req, res) {
+  res.send('article three requesed and will be served here');
+});
+
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
